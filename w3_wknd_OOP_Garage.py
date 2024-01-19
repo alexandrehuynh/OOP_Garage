a0fbb3c1c666961448ed66404de751d9795dc21c
Alex Huynh <alexhuynh@Alexs-MacBook-Pro.local>
2024-01-19 11:06:31 -0800

    first commit

--- w3_wknd_OOP_Garage.py
+++ w3_wknd_OOP_Garage.py
@@ -1,7 +1,7 @@
 class Garage:
     def __init__(self):
         self.tickets = [True] * 121 # Initial ticket
-        self.parking_spaces = [True] * 121 # Initial parking spacesw
+        self.parking_spaces = [True] * 121 # Initial parking spaces
         self.current_ticket = {}
         
     def take_ticket(self):
@@ -11,7 +11,7 @@ class Garage:
             
             self.tickets[ticket_index] = False
             self.parking_spaces[ticket_index] = False
-            self.current_ticket = {"ticket number": ticket_index, "paid": False}
+            self.current_ticket = {"ticket_number": ticket_index, "paid": False}
             print(f"Ticket #{ticket_index} taken. Please park at space #{ticket_index}.")
         
         else:
@@ -22,13 +22,26 @@ class Garage:
         
         if amount > 0:
             self.current_ticket["paid"] = True
-            print(f"Your ticket number: {self.current_ticket['ticket number']} is now paid")
+            print(f"Your ticket number: {self.current_ticket['ticket_number']} is now paid")
     
-    def leave_garage():
-        pass
+    def leave_garage(self):
+        if "ticket_number" in self.current_ticket:
+            if self.current_ticket.get("paid"):
+                print("Thank you, Have a nice day!")
+                ticket_number = self.current_ticket['ticket_number']
+                self.parking_spaces[ticket_number] = True
+                self.tickets[ticket_number] = True
+                self.current_ticket = {}
 
-#####   TESTING    ####
-garage = Garage()
-garage.take_ticket()
-garage.pay_for_parking()
-garage.leave_garage()
+            else:
+                print("You have not paid your ticket.")
+                self.pay_for_parking() 
+                if self.current_ticket.get("paid"):
+                    print("Thank you, Have a nice day!")
+                    ticket_number = self.current_ticket['ticket_number']
+                    self.parking_spaces[ticket_number] = True
+                    self.tickets[ticket_number] = True
+                    self.current_ticket = {}
+                
+        else:
+            print("Yoooo we need a ticket home boy/girl!")
